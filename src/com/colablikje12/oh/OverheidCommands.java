c64e15c23d55c85b52f5510633fda93200502833
MrWouterNL <wouter@minetopiasdb.nl>
2019-01-05 14:05:42 +0100

    Fixed indentation and useless player cast

--- src/com/colablikje12/oh/OverheidCommands.java
+++ src/com/colablikje12/oh/OverheidCommands.java
@@ -10,21 +10,18 @@ public class OverheidCommands implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		Player player = (Player) sender;
 		
-        player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9&m---------------------------------------"));
-        player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&f&lOverheid lijst"));
-        player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9Gemeente: " + ChatColor.WHITE + Main.gemeente.toString().replaceAll("\\[", "").replaceAll("\\]", "")));
-        player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9Inkoop: " + ChatColor.WHITE + Main.inkoop.toString().replaceAll("\\[", "").replaceAll("\\]", "")));
-        player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9Bouwbedrijf: " + ChatColor.WHITE + Main.bouwbedrijf.toString().replaceAll("\\[", "").replaceAll("\\]", "")));
-        player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9Politie: " + ChatColor.WHITE + Main.politie.toString().replaceAll("\\[", "").replaceAll("\\]", "")));
-        player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9Tech: " + ChatColor.WHITE + Main.tech.toString().replaceAll("\\[", "").replaceAll("\\]", "")));
-        player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9Bouwbedrijf TP: " + ChatColor.WHITE + Main.tpbouwbedrijf.toString().replaceAll("\\[", "").replaceAll("\\]", "")));
-        player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9&m---------------------------------------"));
+        	sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9&m---------------------------------------"));
+        	sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&f&lOverheid lijst"));
+        	sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9Gemeente: " + ChatColor.WHITE + Main.gemeente.toString().replaceAll("\\[", "").replaceAll("\\]", "")));
+       		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9Inkoop: " + ChatColor.WHITE + Main.inkoop.toString().replaceAll("\\[", "").replaceAll("\\]", "")));
+       	 	sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9Bouwbedrijf: " + ChatColor.WHITE + Main.bouwbedrijf.toString().replaceAll("\\[", "").replaceAll("\\]", "")));
+        	sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9Politie: " + ChatColor.WHITE + Main.politie.toString().replaceAll("\\[", "").replaceAll("\\]", "")));
+        	sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9Tech: " + ChatColor.WHITE + Main.tech.toString().replaceAll("\\[", "").replaceAll("\\]", "")));
+        	sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9Bouwbedrijf TP: " + ChatColor.WHITE + Main.tpbouwbedrijf.toString().replaceAll("\\[", "").replaceAll("\\]", "")));
+        	sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9&m---------------------------------------"));	
 		
-		
-		
-		return false;
+		return true;
 	}
 
 }
